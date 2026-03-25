720d65f081ea8d407c5ef336a68694da4345678b
Rasmy-RK <rasmyrk06@gmail.com>
2026-03-25 11:06:37 +0530

    Refactor integerReplacement method in Solution class

--- 397- Integer Placement.java
+++ 397- Integer Placement.java
@@ -1,7 +1,7 @@
 class Solution { 
 
     public int integerReplacement(int n) { 
-       long num = n;   // use long to avoid overflow 
+       long num = n;   
         int count = 0; 
         while (num != 1) { 
             if ((num & 1) == 0) { 
